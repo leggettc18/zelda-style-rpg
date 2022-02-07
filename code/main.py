8b6d005cdd0275c37467bae69bb5dcac0093e856
Christopher Leggett <chris@leggett.dev>
2022-02-07 01:34:28 +0000

    Fixes misspelled constant name

--- code/main.py
+++ code/main.py
@@ -15,7 +15,7 @@ class Game:
 
         # general setup
         pygame.init()
-        self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption('Zelda')
         self.clock = pygame.time.Clock()
 
